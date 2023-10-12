451c7b0021813d95f07df363f6e5601ef003aec5
SamuelOsort <samuelosoriotamayo@gmail.com>
2023-10-12 16:21:02 -0500

    v2

--- js/app.js
+++ js/app.js
@@ -43,7 +43,7 @@ const cargarTabla = () => {
         <td>${item.tipo}</td>
         <td>${item.tiempo}</td>
         <td>$${item.precio}</td>
-        <td></td>
+        <td><img src="${item.imagen}"</td>
         <td>
         <div class="d-flex gap-2">
         <button class="btn btn-outline-warning" onclick="mostrarModal(${item.id})"><i class="fa fa-pencil" aria-hidden="true"></i></button>
